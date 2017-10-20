1f960e3ae875e1ca5e2dcc42c1687fe32369b285
umangahuja1 <umangahuja1@gmail.com>
2017-10-21 02:18:40 +0530

    equal to with AC update

--- app/src/main/java/com/example/umang/calculator/MainActivity.java
+++ app/src/main/java/com/example/umang/calculator/MainActivity.java
@@ -23,18 +23,19 @@ public class MainActivity extends AppCompatActivity
 
     public void equalTo(View view)
     {
-        result = computeInfixExpr(answer);
-        if(result%1==0)
+        if(answer !="")
         {
-            answer = String.valueOf((int) result);
-        }
-        else
-        {
-            answer = String.format("%.6f", result);
-            answer = removeTrailingZeros(answer);
-        }
+            result = computeInfixExpr(answer);
+            if (result % 1 == 0) {
+                answer = String.valueOf((int) result);
+            }
+            else {
+                answer = String.format("%.6f", result);
+                answer = removeTrailingZeros(answer);
+            }
 
-        displayMessage(answer);
+            displayMessage(answer);
+        }
     }
 
     public void one(View view)
